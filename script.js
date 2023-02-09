8f368a79ebc56c318e717ce144abe2721e63f322
rdupo <67809219+rdupo@users.noreply.github.com>
2023-02-08 23:50:50 -0600

    first updates from v1 > v3
    initial set of updates to convert from onlyphillips > v3 marketplace front-end

--- script.js
+++ script.js
@@ -48,3 +48,40 @@ function clr(x) {
   var f = $(x).parent().parent().children().first().attr('id');
   $('.phunk-wrapper').removeClass(`${f}hide`);
 } 
+
+//show details
+function deets(x) {
+  $('.attr').text('');
+  $('#interact,#overlay').removeClass('hide-me');
+  var d = "#" + x;
+  var s = $(d).children(0).attr('src');
+  var eye = $(d).attr('data-eyes');
+  var sex = $(d).attr('data-sex');
+  var lip = $(d).attr('data-lips');
+  var hair = $(d).attr('data-hair');
+  var ear = $(d).attr('data-ears');
+  var emo = $(d).attr('data-emo');
+  var bea = $(d).attr('data-beard');
+  var face = $(d).attr('data-face');
+  var mou = $(d).attr('data-mouth');
+  var nec = $(d).attr('data-neck');
+  var che = $(d).attr('data-cheeks');
+  var nos = $(d).attr('data-nose');
+  var tee = $(d).attr('data-teeth');
+  
+  $('.i-phunk-id').text('PHUNK ' + x);
+  $('.i-phunk-img').attr('src',s);
+  $('.i-sex').text('Sex: ' + sex);
+  if (eye != 'None') {$('.i-eyes').text('Eyes: ' + eye)};
+  if (lip != 'None') {$('.i-lips').text('Lips: ' + lip)};
+  if (hair != 'None') {$('.i-hair').text('Hair: ' + hair)};
+  if (ear != 'None') {$('.i-ears').text('Ears: ' + ear)};
+  if (emo != 'None') {$('.i-emo').text('Emotion: ' + emo)};
+  if (bea != 'None') {$('.i-beard').text('Beard: ' + bea)};
+  if (face != 'None') {$('.i-face').text('Face: ' + face)};
+  if (mou != 'None') {$('.i-mouth').text('Mouth: ' + mou)};
+  if (nec != 'None') {$('.i-neck').text('Neck: ' + nec)};
+  if (che != 'None') {$('.i-cheeks').text('Cheeks: ' + che)};
+  if (nos != 'None') {$('.i-nose').text('Nose: ' + nos)};
+  if (tee != 'None') {$('.i-teeth').text('Teeth: ' + tee)};
+}

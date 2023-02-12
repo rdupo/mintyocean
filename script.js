398cf73dbd8d461de3db0f99a25dc26957ef41ea
rdupo <67809219+rdupo@users.noreply.github.com>
2023-02-12 13:42:35 -0600

    remove jquery
    replaced all jquery with vanilla js

--- script.js
+++ script.js
@@ -23,10 +23,9 @@ function reset(x) {
 }
 
 //trait filtering 
-$('select').on('change', function() {
-  var d = $(this).attr('id');
-  var c = `${d}hide`
-  var v = (this.value);
+function af(d) {
+  var c = d.concat('hide');
+  var v = document.getElementById(d).value;
   var ul = document.getElementById("phunky-list");
   var li = ul.getElementsByClassName("phunk-wrapper");
 
@@ -35,53 +34,71 @@ $('select').on('change', function() {
     var p = li[i]
 
     if (txtValue == v) {
-      $(p).removeClass(c)
+      p.classList.remove(c);
 
     } else {
-      $(p).addClass(c); 
+      p.classList.add(c); 
     }
   }
-});
+};
 
 //clear trait filtering
 function clr(x) {
-  var f = $(x).parent().parent().children().first().attr('id');
-  $('.phunk-wrapper').removeClass(`${f}hide`);
+  var i = document.getElementById(x);
+  var f = i.parentElement.parentElement.firstElementChild.getAttribute('id');
+  var elems = document.getElementsByClassName('phunk-wrapper');
+  [].forEach.call(elems, function(el) {
+      el.classList.remove(f.concat('hide'));
+  });
 } 
 
 //show details
 function deets(x) {
-  $('.attr').text('');
-  $('#interact,#overlay').removeClass('hide-me');
-  var d = "#" + x;
-  var s = $(d).children(0).attr('src');
-  var eye = $(d).attr('data-eyes');
-  var sex = $(d).attr('data-sex');
-  var lip = $(d).attr('data-lips');
-  var hair = $(d).attr('data-hair');
-  var ear = $(d).attr('data-ears');
-  var emo = $(d).attr('data-emo');
-  var bea = $(d).attr('data-beard');
-  var face = $(d).attr('data-face');
-  var mou = $(d).attr('data-mouth');
-  var nec = $(d).attr('data-neck');
-  var che = $(d).attr('data-cheeks');
-  var nos = $(d).attr('data-nose');
-  var tee = $(d).attr('data-teeth');
-  
-  $('.i-phunk-id').text('PHUNK ' + x);
-  $('.i-phunk-img').attr('src',s);
-  $('.i-sex').text('Sex: ' + sex);
-  if (eye != 'None') {$('.i-eyes').text('Eyes: ' + eye)};
-  if (lip != 'None') {$('.i-lips').text('Lips: ' + lip)};
-  if (hair != 'None') {$('.i-hair').text('Hair: ' + hair)};
-  if (ear != 'None') {$('.i-ears').text('Ears: ' + ear)};
-  if (emo != 'None') {$('.i-emo').text('Emotion: ' + emo)};
-  if (bea != 'None') {$('.i-beard').text('Beard: ' + bea)};
-  if (face != 'None') {$('.i-face').text('Face: ' + face)};
-  if (mou != 'None') {$('.i-mouth').text('Mouth: ' + mou)};
-  if (nec != 'None') {$('.i-neck').text('Neck: ' + nec)};
-  if (che != 'None') {$('.i-cheeks').text('Cheeks: ' + che)};
-  if (nos != 'None') {$('.i-nose').text('Nose: ' + nos)};
-  if (tee != 'None') {$('.i-teeth').text('Teeth: ' + tee)};
+  var elems = document.getElementsByClassName('attr');
+  [].forEach.call(elems, function(el) {
+      el.textContent=Content='';
+  });
+  document.getElementById('interact').classList.remove('hide-me');
+  document.getElementById('overlay').classList.remove('hide-me');
+  var d = document.getElementById(x);
+  var s = d.firstElementChild.getAttribute('src');
+  var eye = d.getAttribute('data-eyes');
+  var sex = d.getAttribute('data-sex');
+  var lip = d.getAttribute('data-lips');
+  var hair = d.getAttribute('data-hair');
+  var ear = d.getAttribute('data-ears');
+  var emo = d.getAttribute('data-emo');
+  var bea = d.getAttribute('data-beard');
+  var face = d.getAttribute('data-face');
+  var mou = d.getAttribute('data-mouth');
+  var nec = d.getAttribute('data-neck');
+  var che = d.getAttribute('data-cheeks');
+  var nos = d.getAttribute('data-nose');
+  var tee = d.getAttribute('data-teeth');
+  document.getElementById('i-phunk-id').textContent=Content='PHUNK ' + x;
+  document.getElementById('i-phunk-img').setAttribute('src',s);
+  document.getElementById('i-sex').textContent=Content = 'Sex: ' + sex;
+  if (eye != 'None') {document.getElementById('i-eyes').textContent='Eyes: ' + eye};
+  if (lip != 'None') {document.getElementById('i-lips').textContent='Lips: ' + lip};
+  if (hair != 'None') {document.getElementById('i-hair').textContent='Hair: ' + hair};
+  if (ear != 'None') {document.getElementById('i-ears').textContent='Ears: ' + ear};
+  if (emo != 'None') {document.getElementById('i-emo').textContent='Emotion: ' + emo};
+  if (bea != 'None') {document.getElementById('i-beard').textContent='Beard: ' + bea};
+  if (face != 'None') {document.getElementById('i-face').textContent='Face: ' + face};
+  if (mou != 'None') {document.getElementById('i-mouth').textContent='Mouth: ' + mou};
+  if (nec != 'None') {document.getElementById('i-neck').textContent='Neck: ' + nec};
+  if (che != 'None') {document.getElementById('i-cheeks').textContent='Cheeks: ' + che};
+  if (nos != 'None') {document.getElementById('i-nose').textContent='Nose: ' + nos};
+  if (tee != 'None') {document.getElementById('i-teeth').textContent='Teeth: ' + tee};
+}
+
+function hide() {
+document.getElementById('interact').classList.add('hide-me'); 
+document.getElementById('overlay').classList.add('hide-me');
+}
+
+function togs() {
+  document.getElementById('view').classList.toggle('hide-me');
+  document.getElementById('hide').classList.toggle('hide-me');
+  document.getElementById('filters').classList.toggle('hide-me');
 }
